b3338b2a26690c0ce46028ac09521005912e02d9
julietab231 <julietab231es@gmail.com>
2023-07-10 16:13:16 +0200

    airflow changes

--- dags/dag01.py
+++ dags/dag01.py
@@ -1,61 +1,96 @@
-################################################
-################################################
-################################################
-# descriptive analysis
-################################################
-################################################
-################################################
 
-'''
-At first run this comand in your environment:
-pip install -r requirements.txt
-'''
+import json
+import subprocess
 
-################################################
-# install libraries
-################################################
+import pendulum
+
 import pandas as pd
 
-################################################
-# read data
-################################################
+from airflow.decorators import dag, task
+import logging
 
-# Define the name of the CSV file
-file_name = "SDG-Case-Study//dataset.csv"
+# get the airflow.task logger
+task_logger = logging.getLogger("airflow.task")
 
-# Read the CSV file from the mounted volume using Pandas
-df = pd.read_csv(file_name,
-                 delimiter=';',
-                 error_bad_lines=False)
 
-################################################
-# data analysis
-################################################
+@dag(
+    schedule=None,
+    start_date=pendulum.datetime(2023, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["analysis"],
+)
+def analysis_taskflow_api():
+    """
+    ### TaskFlow API Analysis Documentation
+    This is a first descriptive analysis of data.
+    """
+    @task()
+    def extract():
+        """
+        #### read data
 
-# Print head of data
-print('Head:\n',df.head())
+        """
+        # Define the name of the CSV file
+        file_name = "SDG-Case-Study//dataset.csv"
 
-# check the shape of the DataFrame
-print('\nShape of the DataFrame:\n', df.shape)  
+        # Read the CSV file from the mounted volume using Pandas
+        df = pd.read_csv(file_name,
+                         delimiter=';',
+                         error_bad_lines=False)
+        task_logger.info('Dataset csv readed')
+        task_logger.info(df.head())
 
-# check the data types of each column
-df_dtypes = df.dtypes
-df_dtypes.to_csv('SDG-Case-Study//df_dtypes.csv')
-print('\nData types of each column:\n', df.dtypes)
+        return df
 
-# get summary statistics of the numerical/date format column
-df_describe = df.describe()
-df_describe.to_csv('SDG-Case-Study//df_describe.csv')
-print('\nSummary statistics \n',df.describe()) 
+    @task()
+    def import_db():
+        """
+        import dataset in sqlite db
+        """
+        cmd = '''sqlite3 dataset.db 
+                -cmd ".output /db/dataset/mysession.txt"
+                -cmd ".log /db/dataset/mycommands.sql"
+                -cmd ".read /db/dataset/schema.sql" 
+                -cmd ".mode csv" 
+                -cmd ".separator ;" 
+                -cmd ".import dataset.csv dataset" 
+                -cmd ".exit" '''
+        output = subprocess.check_output(cmd, shell=True)
 
-# count the number of unique values in each column
-print('\nUnique values in each column:\n', df.nunique())
+        task_logger.info('Dataset imported into dataset.db sqlite3 ')
+        task_logger.info(output.decode('utf-8'))
+        return output.decode('utf-8')
 
-# check the frequency distribution of the 'churn' column
-print('\nDistribution of the churn column:\n', df['churn'].value_counts())
-# churn takes values from 0 to 1
-# mean of churn is 0.49
-# Distribution of the churn column:
-# 0    50438 -- 50%
-# 1    49562 -- 50%
+    @task(multiple_outputs=True)
+
+    def summarise(df):
+        """
+        #### Descriptive analysis
+        """
+
+        # Print head of data
+        print('Head:\n',df.head())
+
+        # check the shape of the DataFrame
+        print('\nShape of the DataFrame:\n', df.shape)  
+
+        # check the data types of each column
+        df_dtypes = df.dtypes
+        df_dtypes.to_csv('SDG-Case-Study//df_dtypes.csv')
+        print('\nData types of each column:\n', df.dtypes)
+
+        # get summary statistics of the numerical/date format column
+        df_describe = df.describe()
+        df_describe.to_csv('SDG-Case-Study//df_describe.csv')
+        print('\nSummary statistics \n',df.describe()) 
+
+        # count the number of unique values in each column
+        print('\nUnique values in each column:\n', df.nunique())
+
+        # check the frequency distribution of the 'churn' column
+        print('\nDistribution of the churn column:\n', df['churn'].value_counts())
+
+        return {"Descriptive statistics": df.describe()}
+analysis_taskflow_api()
+
 
