fc46a2aee429b675fda6a3bdfbeeafa26c2633d5
julietab231 <julietab231es@gmail.com>
2023-07-26 23:10:51 +0200

    small changes

--- dags/analysis.py
+++ dags/analysis.py
@@ -355,7 +355,6 @@ def analysis():
         y = df['churn']
 
         with mlflow.start_run() as run:
-            mlflow.set_tag('random forest')
 
             # Split the data into training and testing sets
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
